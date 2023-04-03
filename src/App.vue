96d7ecc6f006d60c1b51c72da54e3a3d3cfd82ea
jxch <15553598935@163.com>
2023-04-03 14:23:47 +0800

    APP主界面

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,14 @@
 <template>
-    <n-layout has-sider>
-        <n-layout-sider
-            bordered
-            collapse-mode="width"
-            :collapsed-width="64"
-            :width="240"
-            :collapsed="collapsed"
-            show-trigger
-            @collapse="collapsed = true"
-            @expand="collapsed = false"
-        >
-            <n-menu
-                :collapsed="collapsed"
-                :collapsed-width="64"
-                :collapsed-icon-size="22"
-                :options="menuOptions"
-                key-field="whateverKey"
-                label-field="whateverLabel"
-                children-field="whateverChildren"
-            />
-        </n-layout-sider>
-        <n-layout />
-    </n-layout>
+    <n-notification-provider>
+        <n-message-provider>
+            <router-view />
+        </n-message-provider>
+    </n-notification-provider>
 </template>
 
 <script lang="ts">
-import { defineComponent, h, ref, Component } from 'vue'
-import { NIcon, NLayout, NLayoutSider, NMenu } from 'naive-ui'
-import type { MenuOption } from 'naive-ui'
-import {
-    Calculator as Calculator,
-    TrendingUp as TrendingUp
-} from '@vicons/ionicons5'
-
-function renderIcon (icon: Component) {
-    return () => h(NIcon, null, { default: () => h(icon) })
-}
-
-const menuOptions: MenuOption[] = [
-    {
-        whateverLabel: '计算器',
-        whateverKey: 'calculator',
-        icon: renderIcon(Calculator),
-        disabled: false,
-        whateverChildren: [
-            {
-                whateverLabel: '交易计算器',
-                icon: renderIcon(TrendingUp),
-                whateverKey: 'trading-calculator'
-            }
-        ]
-    }
-]
-
+import { defineComponent } from 'vue'
 export default defineComponent({
-    components: {
-        NLayout,
-        NLayoutSider,
-        NMenu
-    },
-    setup () {
-        return {
-            collapsed: ref(true),
-            menuOptions
-        }
-    }
+    name: 'App',
 })
 </script>
