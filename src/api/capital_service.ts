834f38ad0dac73dbc7da5ecc7c80f7850f7de490
jxch <qianbuhan@proton.me>
2023-05-17 20:35:14 +0800

    更新服务器域名

--- src/api/capital_service.ts
+++ src/api/capital_service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const capital_service_api = axios.create({
-    baseURL: 'http://jiangxicheng.xyz:15000/',
+    baseURL: 'http://jiangxicheng.site:15000/',
     timeout: 30000,
     headers: {'Accept': 'application/json', 'Content-Type': 'application/json;charset=utf-8'}
 })
