ce755d3cf0be44c357253d17c84cb88fd929eb64
jxch <15553598935@163.com>
2023-06-19 10:35:50 +0800

    重构

--- src/views/bubble/MarketBubble.vue
+++ src/views/bubble/MarketBubble.vue
@@ -3,7 +3,7 @@
     <n-card size="small" hoverable style="height: 8%">
       <n-space :vertical="false" size="small" justify="space-between">
         <n-space>
-          <date-selector :condition="condition"/>
+          <stock-pool-selector v-model:k-lines="stockChart.kLines"/>
           <n-form-item label="中期百分比：" size="small" label-placement="left">
             <n-slider style="width: 100px" :default-value="stockChart.midPercent" :step="1"
                       :min="10" :max="80" v-model:value="stockChart.midPercent"
@@ -14,11 +14,6 @@
                       :min="1" :max="50" v-model:value="stockChart.shortPercent"
                       :format-tooltip="formatPercentTooltip"/>
           </n-form-item>
-          <n-dropdown size="small" trigger="hover" :options="stockPoolOptionsComputed" @select="stockPoolHandleSelect">
-            <n-button size="small" :loading="condition.loading" @click="update">
-              {{ condition.stockPoolSelectLabel }}
-            </n-button>
-          </n-dropdown>
         </n-space>
         <n-space>
           <p>纵轴为长期趋势，横轴为中期趋势，散点大小和颜色为短期趋势</p>
@@ -39,80 +34,25 @@ import DateSelector from "cc/DateSelector.vue";
 import dayjs from "dayjs";
 import MarketScatterChart from "vv/bubble/MartketScatterChart.vue";
 import {useMessage} from "naive-ui";
+import StockPoolSelector from "cc/StockPoolSelector.vue";
 
 export default defineComponent({
   name: "MarketBubble",
-  components: {MarketScatterChart, DateSelector},
+  components: {StockPoolSelector, MarketScatterChart, DateSelector},
 
   setup() {
-    const message = useMessage()
-    const template = "YYYY-MM-DD"
-    const condition = reactive({
-      last: 3,
-      cycle: 'month',
-      stockPool: [],
-      loading: false,
-      stockPoolSelectLabel: '',
-      stockPoolSelectKey: '',
-    })
     const stockChart = reactive({
       kLines: '',
       midPercent: 40,
       shortPercent: 15,
     })
 
-    const stockPoolOptionsComputed = computed(() => {
-      const rawStockPoolOptions = toRaw(condition.stockPool)
-      return Object.keys(rawStockPoolOptions).map(key => {
-        return {label: rawStockPoolOptions[key]['alias'], key: key}
-      })
-    })
-
-    function queryStockPool() {
-      apis.capital_service_apis.query_stock_pool().then((res) => {
-        condition.stockPool = res['data']
-        const keys = Object.keys(condition.stockPool)
-        stockPoolHandleSelect(keys[keys.length - 1])
-      }).catch((e) => {
-        console.log(e)
-        message.warning('请检查网络并稍后重试，或查看控制台报错信息：' + e['message'])
-      })
-    }
-
-    function update() {
-      condition.loading = true
-      apis.capital_service_apis.query_k_json_by_stock_pool({
-        "stock_pool": condition.stockPoolSelectKey,
-        "start": dayjs(new Date()).subtract(condition.last, condition.cycle).format(template),
-        "end": dayjs(new Date()).format(template)
-      }).then((res) => {
-        stockChart.kLines = res['data']
-      }).catch((e) => {
-        console.log(e)
-        message.warning('请检查网络并稍后重试，或查看控制台报错信息')
-      }).finally(() => {
-        condition.loading = false
-      })
-    }
-
-    function stockPoolHandleSelect(key) {
-      condition.stockPoolSelectKey = key
-      condition.stockPoolSelectLabel = condition.stockPool[key]['alias']
-      update()
-    }
-
-    queryStockPool()
-
     function formatPercentTooltip(value) {
       return `${value}%`
     }
 
     return {
-      stockPoolOptionsComputed,
-      stockPoolHandleSelect,
       formatPercentTooltip,
-      condition,
-      update,
       stockChart,
     }
   }
