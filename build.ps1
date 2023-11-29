c61632df6a6fdc9062862bb80ba3c5543799fc34
jxch <qianbuhan@proton.me>
2023-11-29 19:23:30 +0800

    docker buildx create

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
 npm run build
+# docker buildx create --use
 docker buildx build --platform=linux/arm64,linux/amd64 -t jxch/capital-web:$(Get-Date -Format 'yyyyMMdd') -t jxch/capital-web:latest . --push
